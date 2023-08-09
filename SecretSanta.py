b7d74cecd4e16dc0749cd4656550e37cf68c5a1f
James <>
2023-08-09 00:56:49 -0700

    Add empty array to names.json and add comments in SecretSanta.py

--- SecretSanta.py
+++ SecretSanta.py
@@ -1,9 +1,16 @@
 import json
 import random
+
+#Open the adjacent names.json file.
 with open("names.json", "r") as names_file:
+    #Load it into the variable data.
     data = json.load(names_file)
+#Get the names from the array.
 names = data["names"]
+#Shuffle the array.
 random.shuffle(names)
 
+#Iterate over the array.
 for i in range(len(names)):
-    print(names[i], " gets a gift for ", names[(i + 1)%len(names)])
+    #The name at index i gets a gift for the name at index i+1. This guarantees fairness, no need for redraws.
+    print(names[i], " gets a gift for ", names[(i + 1) % len(names)])
